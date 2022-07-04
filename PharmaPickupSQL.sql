d1cd219a7b4ae23a088f1348ad2b89ed17c304dd
Kar-Sha <76408122+Kar-Sha@users.noreply.github.com>
2022-07-03 20:42:40 -0700

    Fix Syntax
    MySQL Workbench import works properly now

--- PharmaPickupSQL.sql
+++ PharmaPickupSQL.sql
@@ -2,21 +2,23 @@ DROP SCHEMA IF EXISTS pharmapickup;
 CREATE SCHEMA pharmapickup;
 USE pharmapickup;
 
-CREATE TABLE patients (
-  patient_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(32) NOT NULL,
-  last_name VARCHAR(32) NOT NULL,
-  username VARCHAR(32) UNIQUE NOT NULL,
-  email VARCHAR (256) NOT NULL,
-  password VARCHAR(60) UNIQUE NOT NULL,
-  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (patient_id),
-) ENGINE=InnoDB;
+CREATE TABLE `pharmapickup`.`patient` (
+  `patient_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `create_date` DATETIME NOT NULL,
+  PRIMARY KEY (`patient_id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
+  UNIQUE INDEX `password_UNIQUE` (`password` ASC) VISIBLE) ENGINE=InnoDB;
 
-CREATE TABLE staff (
-  staff_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  username VARCHAR(32) UNIQUE NOT NULL,
-  password VARCHAR(60) UNIQUE NOT NULL,
-  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (staff_id),
-) ENGINE=InnoDB;
+
+CREATE TABLE `pharmapickup`.`staff` (
+  `staff_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `create_date` DATETIME NOT NULL,
+  PRIMARY KEY (`staff_id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
+  UNIQUE INDEX `password_UNIQUE` (`password` ASC) VISIBLE) ENGINE=InnoDB;
