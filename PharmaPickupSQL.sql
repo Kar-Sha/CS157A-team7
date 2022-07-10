f7eacd757fc0c4e5ca3c6c2bad12bc043e058889
Kar-Sha <76408122+Kar-Sha@users.noreply.github.com>
2022-07-09 18:37:28 -0700

    fixed syntax of comments

--- PharmaPickupSQL.sql
+++ PharmaPickupSQL.sql
@@ -2,13 +2,13 @@ DROP SCHEMA IF EXISTS pharmapickup;
 CREATE SCHEMA pharmapickup;
 USE pharmapickup;
  
---Table for pharmacy (Entity Set)
+-- Table for pharmacy (Entity Set)
 CREATE TABLE pharmacy (
   pharmacy_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(32) NOT NULL,
   location VARCHAR(255) UNIQUE NOT NULL,
   PRIMARY KEY (pharmacy_id)) ENGINE=InnoDB;
---Data Instances
+-- Data Instances
   INSERT INTO pharmacy(pharmacy_id, name, location)
   VALUES(1, 'Kaiser', '123 Kaiser Road'),
   (2, 'CVS', '100 CVS Drive'),
@@ -21,7 +21,7 @@ CREATE TABLE pharmacy (
   (9, 'CVS', '4550 Meridian Ave'), 
   (10, 'Walgreens', '1376 Kooser Rd'); 
 
---Table for patient (Entity Set)
+-- Table for patient (Entity Set)
 CREATE TABLE patient (
   patient_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(32) NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE patient (
   password VARCHAR(60) UNIQUE NOT NULL,
   create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (patient_id)) ENGINE=InnoDB;
---Data Instances
+-- Data Instances
   INSERT INTO patient(patient_id, first_name, last_name, username, email, password, create_date)
   VALUES(1, 'Bob', 'Marley', 'bmarley123', 'bobmarley@yahoo.com', 'bobpass123', '2007-02-15 04:34:33'),
   (2, 'Maddison', 'Walle', 'madmed', 'maddison.walle@gmail.com', 'madiwall', '2008-07-22 06:54:53'),
@@ -44,7 +44,7 @@ CREATE TABLE patient (
   (9, 'Tony', 'Chopper', 'opfan5', 'tonytonychopper@onepiece.com', 'iloveop', '2014-12-25 12:25:31'),
   (10, 'Michael', 'Gold', 'goldenboy', 'michaelgold@gmail.com', 'aumetal', '2015-09-09 09:09:09');
  
---Table for staff (Entity Set)
+-- Table for staff (Entity Set)
 CREATE TABLE staff (
   staff_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   pharmacy_id INT UNSIGNED NOT NULL,
@@ -53,7 +53,7 @@ CREATE TABLE staff (
   create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (staff_id),
   FOREIGN KEY(pharmacy_id) REFERENCES pharmacy(pharmacy_id)) ENGINE=InnoDB;
---Data Instances
+-- Data Instances
   INSERT INTO staff(staff_id, pharmacy_id, username, password, create_date)
   VALUES(1, 1, 'admin', 'adminpass', '2006-01-01 01:01:01'),
   (2, 2, 'pharmacy', 'pharmapass', '2006-01-02 02:02:02'),
@@ -66,13 +66,13 @@ CREATE TABLE staff (
   (9, 9, 'bandage', 'bandpass', '2006-09-09 09:09:09'),
   (10, 10, 'heal', 'healpass', '2006-10-10 10:10:10');
  
---Table for medicine (Entity Set)
+-- Table for medicine (Entity Set)
 CREATE TABLE medicine (
   medicine_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(32) UNIQUE NOT NULL,
   dosage VARCHAR(60) NOT NULL,
   PRIMARY KEY (medicine_id)) ENGINE=InnoDB;
---Data Instances
+-- Data Instances
   INSERT INTO medicine(medicine_id, name, dosage)
   VALUES(1, 'Advil', '200-400mg'),
   (2, 'Melatonin', '1-5mg'),
@@ -85,12 +85,12 @@ CREATE TABLE medicine (
   (9, 'Vaseline', 'Any Amount Necessary'),
   (10, 'Band-Aid', '1 piece');
  
---Table for category (Entity Set)
+-- Table for category (Entity Set)
 CREATE TABLE category (
   category_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(32) UNIQUE NOT NULL,
   PRIMARY KEY (category_id)) ENGINE=InnoDB;
---Data Instances
+-- Data Instances
   INSERT INTO category(category_id, name)
   VALUES(1, 'Fever'),
   (2, 'Pain Relief'),
@@ -105,13 +105,13 @@ CREATE TABLE category (
   (11, 'Skin Irritation'),
   (12, 'Miscellaneous');
  
---Table for different categories that medicine falls under (Relationship)
+-- Table for different categories that medicine falls under (Relationship)
 CREATE TABLE medicine_category (
   medicine_id INT UNSIGNED NOT NULL,
   category_id INT UNSIGNED NOT NULL,
   FOREIGN KEY(medicine_id) REFERENCES medicine(medicine_id),
   FOREIGN KEY(category_id) REFERENCES category(category_id)) ENGINE=InnoDB;
---Data Instances
+-- Data Instances
   INSERT INTO medicine_category(medicine_id, category_id)
   VALUES(1,1),
   (1,2),
@@ -125,7 +125,7 @@ CREATE TABLE medicine_category (
   (7, 1),
   (7, 4);
 
---Table for patient's prescriptions (Relationship)
+-- Table for patient's prescriptions (Relationship)
 CREATE TABLE prescription (
   patient_id INT UNSIGNED NOT NULL,
   medicine_id INT UNSIGNED NOT NULL,
@@ -133,7 +133,7 @@ CREATE TABLE prescription (
   approval_status VARCHAR(32) DEFAULT 'Pending',
   FOREIGN KEY(patient_id) REFERENCES patient(patient_id),
   FOREIGN KEY(medicine_id) REFERENCES medicine(medicine_id)) ENGINE=InnoDB;
---Data Instances
+-- Data Instances
   INSERT INTO prescription(patient_id, medicine_id, quantity, approval_status)
   VALUES(1, 1, 3, 'Approved'),
   (1, 3, 4, 'Approved'),
@@ -146,14 +146,14 @@ CREATE TABLE prescription (
   (9, 7, 2, 'Approved'),
   (9, 8, 1, 'Approved');
 
---Table for medicine stock within different pharmacies (Relationship)
+-- Table for medicine stock within different pharmacies (Relationship)
 CREATE TABLE medicine_stock (
   medicine_id INT UNSIGNED NOT NULL,
   pharmacy_id INT UNSIGNED NOT NULL,
   quantity INT,
   FOREIGN KEY(medicine_id) REFERENCES medicine(medicine_id),
   FOREIGN KEY(pharmacy_id) REFERENCES pharmacy(pharmacy_id)) ENGINE=InnoDB;
- --Data Instances
+ -- Data Instances
   INSERT INTO medicine_stock(medicine_id, pharmacy_id, quantity)
   VALUES(1, 1, 50),
   (2, 2, 50),
@@ -167,12 +167,12 @@ CREATE TABLE medicine_stock (
   (9, 9, 25),
   (10, 2, 5);
 
---Table for ingredients that patients are allergic to (Relationship)
+-- Table for ingredients that patients are allergic to (Relationship)
 CREATE TABLE patient_allergies (
   patient_id INT UNSIGNED NOT NULL,
   allergy VARCHAR(64) NOT NULL,
   FOREIGN KEY(patient_id) REFERENCES patient(patient_id)) ENGINE=InnoDB;
---Data Instances
+-- Data Instances
 INSERT INTO patient_allergies(patient_id, allergy)
   VALUES(2,'Gluten'),
   (2, 'Lactose'),
@@ -185,12 +185,12 @@ INSERT INTO patient_allergies(patient_id, allergy)
   (9, 'Paracetamol'),
   (10, 'Peanut');
 
---Table for ingredients (Entity Set)
+-- Table for ingredients (Entity Set)
 CREATE TABLE ingredient (
   ingredient_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(64) UNIQUE NOT NULL,
   PRIMARY KEY (ingredient_id)) ENGINE=InnoDB;
---Data Instances
+-- Data Instances
 INSERT INTO ingredient(ingredient_id, name)
   VALUES(1, 'Lactose'),
   (2, 'Gluten'),
@@ -203,13 +203,13 @@ INSERT INTO ingredient(ingredient_id, name)
   (9, 'Atorvastatin'),
   (10, 'Ascorbic acid');
 
---Table of ingredients within medicines (Relationship)
+-- Table of ingredients within medicines (Relationship)
 CREATE TABLE medicine_ingredients (
   medicine_id INT UNSIGNED NOT NULL,
   ingredient_id INT UNSIGNED NOT NULL,
   FOREIGN KEY(medicine_id) REFERENCES medicine(medicine_id),
   FOREIGN KEY(ingredient_id) REFERENCES ingredient(ingredient_id)) ENGINE=InnoDB;
- --Data Instances
+ -- Data Instances
   INSERT INTO medicine_ingredients(medicine_id, ingredient_id)
   VALUES(1, 2),
   (1,3),
